ac1a461048463479858e10887809dd4157c36ec3
codeshootah <vnukovg@gmail.com>
2019-11-05 02:23:53 +0300

    first one

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Signin from '@/components/Signin'
+import Signup from '@/components/Signup'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/',
+      name: 'Signup',
+      component: Signup
+    }
+  ]
+})
